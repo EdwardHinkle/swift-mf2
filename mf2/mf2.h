2d213166af3935a53c393cbe10d8c5cff5d040c9
Edward Hinkle <eddie@meetmaestro.com>
2017-04-24 00:56:13 -0400

    Initial Commit

---
+++ mf2/mf2.h
@@ -0,0 +1,19 @@
+//
+//  mf2.h
+//  mf2
+//
+//  Created by Eddie Hinkle on 4/24/17.
+//  Copyright © 2017 Studio H, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for mf2.
+FOUNDATION_EXPORT double mf2VersionNumber;
+
+//! Project version string for mf2.
+FOUNDATION_EXPORT const unsigned char mf2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mf2/PublicHeader.h>
+
+
